7dfc394ea4af1049cc54efd266c725baa83147e0
Beqa <b.chkhaidze@palitra.ge>
2021-12-16 12:23:29 +0400

    updated font size

--- modules/index.js
+++ modules/index.js
@@ -1,5 +1,8 @@
 export const formatPrint = (msg, { color = "red", fontWeight = "bold" } = {}) =>
-  console.log(`%c${msg}`, `color: ${color}; font-weight: ${fontWeight}`);
+  console.log(
+    `%c${msg}`,
+    `color: ${color}; font-weight: ${fontWeight}; font-size: 3em;`
+  );
 
 export const mota = (name = "hota") => {
   formatPrint(`${name} mota`);
